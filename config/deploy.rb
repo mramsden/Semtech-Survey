ee0b14d6de23b3907414a70c42a280c64856b798
Marcus Ramsden <marcus@beyondthecorner.co.uk>
2009-06-16 17:17:33 +0000

    Updated the maintenance page.

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,33 +9,34 @@ set :use_sudo, false
 
 set :domain, "iamvm-mmr2.ecs.soton.ac.uk"
 set :user, "mmr"
-set :password, Capistrano::CLI.password_prompt("Type the password for #{user}@#{domain}")
 set :port, 22
 server domain, :app, :web
 role :db, domain, :primary => true
 
-set :repository,  "svn+ssh://iamvm-mmr2.ecs.soton.ac.uk/var/svn/semtech-survey"
-set :scm_username, "mmr"
-set :scm_password, Capistrano::CLI.password_prompt("Type your svn password for user #{scm_username}: ")
+set :repository,  "svn+ssh://#{user}@iamvm-mmr2.ecs.soton.ac.uk/var/svn/semtech-survey/trunk"
 set :deploy_via, :export
 
 after :deploy, 'deploy:cleanup'
 
-desc "Put the site into maintenence mode."
-maintenance.task :enable, :roles => :app do
-  run "cp #{release_path}/public/maintenance.htaccess #{release_path}/public/.htaccess"
+namespace :maintenance do
+  desc "Put the site into maintenence mode."
+  task :enable, :roles => :app do
+    run "cp #{release_path}/public/maintenance.htaccess #{release_path}/public/.htaccess"
+  end
+
+  desc "Take the site out of maintenence mode."
+  task :disable, :roles => :app do
+    run "cp #{release_path}/public/live.htaccess #{release_path}/public/.htaccess"
+  end
 end
 
-desc "Take the site out of maintenence mode."
-maintenance.task :enable, :roles => :app do
-  run "cp #{release_path}/public/live.htaccess #{release_path}/public/.htaccess"
-end
+namespace :deploy do
+  desc "Do nothing"
+  task :restart, :roles => :app do
+    # PHP apps don't need the server to be restarted.
+  end
 
-desc "Do nothing"
-deploy.task :restart, :roles => :app do
-  # PHP apps don't need the server to be restarted.
-end
-
-deploy.task :migrate, :roles => :app do
-  # Migrate is Rails specific.
+  task :migrate, :roles => :app do
+    # Migrate is Rails specific.
+  end
 end
