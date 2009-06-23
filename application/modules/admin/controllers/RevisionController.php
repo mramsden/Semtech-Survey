11bca0b5d5dd4851e08e8fa8484f784a4726c5b6
Marcus Ramsden <marcus@beyondthecorner.co.uk>
2009-06-23 15:32:30 +0000

    You can now delete a revision.

--- application/modules/admin/controllers/RevisionController.php
+++ application/modules/admin/controllers/RevisionController.php
@@ -28,11 +28,28 @@ class Admin_RevisionController extends Semtech_Controller_Admin_Action
       $revision = Semtech_Model_Revision::getRevision($revisionid);
       if (!is_null($revision))
       {
-        $this->view->revision = $revision;
+        if ($this->getRequest()->isPost())
+        {
+          $techid = $revision->getTechnology()->id;
+          if ($this->getRequest()->getParam("confirmation") == "Confirm Action")
+          {
+            $revision->delete();
+            $this->_flashMessenger->addMessage("Revision deleted.");
+          }
+          else
+          {
+            $this->_flashMessenger->addMessage("Delete action cancelled.");
+          }
+          $this->getHelper("Redirector")->gotoSimple("view", "technology", "semtech", array("techid" => $techid));
+        }
+        else
+        {
+          $this->view->revision = $revision;
+        }
       }
       else
       {
-        Zend_Registry::get("log")->err(__METHOD__.": Unable to find revision ".$revisionid.".");
+        Zend_Registry::get("log")->crit(__METHOD__.": Unable to find revision ".$revisionid.".");
         $this->_flashMessenger->addMessage("Unable to find requested revision.");
         $this->getHelper("Redirector")->gotoUrl($_SERVER['HTTP_REFERER']);
       }
