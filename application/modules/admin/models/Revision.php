2ae47102ab197bc942244cf1f2b0e3a14729db78
Marcus Ramsden <marcus@beyondthecorner.co.uk>
2009-06-23 16:15:56 +0000

    Finalized revision deletion to ensure that a message states if there are no revisions available.

--- application/modules/admin/models/Revision.php
+++ application/modules/admin/models/Revision.php
@@ -3,6 +3,28 @@ class Admin_Model_Revision extends Zend_Db_Table_Row
 {
 	
 	/**
+   * This method will fetch the requested revision.
+   * 
+   * @param int $revid
+   * @param int $techid
+   * @param int $userid
+   * @param boolean $original
+   * @return Semtech_Model_Revision
+   */
+  public static function getRevision($revid = null, $techid = null, $userid = null, $original = false)
+  {
+    $trt = new Semtech_Model_DbTable_TechnologyRevisions();
+    $select = $trt->select()->where("original = ?", $original);
+    if ($revid)
+      $select->where("id = ?", $revid);
+    if ($techid)
+      $select->where("technology = ?", $techid);
+    if ($userid)
+      $select->where("createdby = ?", $userid);
+    return $trt->fetchRow($select);
+  }
+	
+	/**
 	 * This function ensures that the databases referential integrity is
 	 * maintained. When the revision is deleted the associated tags should
 	 * also be deleted. This stops orphaned tag references from appearing.
